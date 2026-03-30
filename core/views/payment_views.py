3badb783897c1e5deb987ea575af9c3c216d10e1
appiahkubis14 <appiahkubis14@gmail.com>
2026-03-30 20:16:06 +0000

    u

--- core/views/payment_views.py
+++ core/views/payment_views.py
@@ -46,7 +46,7 @@ def _get_client_ip(request: HttpRequest) -> str:
 
 def get_tracked_payment_link(bill_type: str, bill_number: str, link_type: str = 'web') -> str:
     """Return a redirect URL that tracks the click before forwarding to the payment page."""
-    base_url = getattr(settings, 'BASE_URL', 'http://lanmma.sktlive.com/').rstrip('/')
+    base_url = getattr(settings, 'BASE_URL', 'https://lanmma.sktlive.com/').rstrip('/')
     return f"{base_url}/pay/l/{bill_type}/{bill_number}/{link_type}/"
 
 
